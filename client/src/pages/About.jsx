cfb819f5ec63d6b1cb706df38374afccf1137148
Mohannad Alzayak <mohannad.alzayak2@gmail.com>
2024-06-26 01:59:36 +0200

    Last version

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
-
-function About(props) {
+export default function About() {
     return (
-        <div>gsdgsdgsdg</div>
+        <div className='min-h-screen flex items-center justify-center'>
+            <div className='max-w-2xl mx-auto p-3 text-center'>
+                <div>
+                    <h1 className='text-3xl font font-semibold text-center my-7'>
+
+                    </h1>
+                    <div className='text-md text-gray-500 flex flex-col gap-6'>
+
+                     <p>   Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                           At vero eos et accusam et justo duo dolores et ea rebum.
+                           Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                     </p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
-
-export default About;
